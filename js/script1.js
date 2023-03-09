6b4b06da887cc48fedb93701b2ca09c1e54692b5
IVANCSDO <114605010+IVANCSDO@users.noreply.github.com>
2023-03-09 08:42:10 +0100

    Método de saltar
    pues eso

--- js/script1.js
+++ js/script1.js
@@ -7,8 +7,38 @@ class Rana {
         this.imagenParado = imagenParado;
         this.imagenMovimiento = imagenMovimiento;
         this.imagenMuerto = imagenMuerto;
+        //velocidad, simula la gravedad, bajando constantemente una velocidad
+        this.velocidad = -5;
+        this.contadorSalto = 0;
+
     }
     //Aqui meteriamos los métodos que queremos aplicar
+
+    //método que ejecuta el bucle que "salta"
+    saltar(){
+
+        this.contadorSalto = 0;
+
+        this.bucle()
+
+    }
+
+    bucle(){
+        //cuando la variable sea undefined acaba la ejecución del bucle
+        //no estoy seguro de que pueda leer la variable
+        if(this.contadorSalto = undefined){
+            return
+        }else if(this.contadorSalto<1e3){
+            this.contadorSalto++;
+            this.velocidad = 5
+            setTimeout(bucle, 50)
+        }else{
+            this.velocidad = -5
+            if (this.contadorSalto = 2e3) {this.contadorSalto = undefined}
+            setTimeout(bucle, 50)
+        }
+    }
 }
-     let Pepe = new Rana('Pepe','postionX','positionY','imagenParado','imagenMovimiento','imagenMuerto'); //Creamos el objeto con los parametros que queremos meterle
+
+    let Pepe = new Rana('Pepe','postionX','positionY','imagenParado','imagenMovimiento','imagenMuerto'); //Creamos el objeto con los parametros que queremos meterle
     //Aquí ejecutariamos los metodos que hemos creado arriba
