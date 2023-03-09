0b63d00c50c4af28c990adb4746584457d50f729
Rampayan <85584185+Rampayan@users.noreply.github.com>
2023-03-09 13:04:35 +0100

    rayan

--- js/scriptniveles.js
+++ js/scriptniveles.js
@@ -1,16 +1,14 @@
 class Nivel {
-       //Atributos
-    constructor(numero, velocidad, fondo) {
-      this.numero = numero;
-      this.velocidad = velocidad;
-      this.fondo = fondo;
-    }
-     //Aqui meteriamos los métodos que queremos aplicar
+  constructor(numero, velocidad, fondo) {
+    this.numero = numero;
+    this.velocidad = velocidad;
+    this.fondo = fondo;
   }
+}
 
-var nivel1 = new Nivel(1, 10);
-var nivel2 = new Nivel(2, 15);
-var nivel3 = new Nivel(3, 20);
+var nivel1 = new Nivel(1, 10, "fondo1.jpg");
+var nivel2 = new Nivel(2, 15, "fondo2.jpg");
+var nivel3 = new Nivel(3, 20, "fondo3.jpg");
 
 // Cuando la rana llegue al final del nivel 1, cambia al nivel 2
 if (Pepe.llegaAlFinal(nivel1)) {
