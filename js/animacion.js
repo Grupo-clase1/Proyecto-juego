f868e0ab3bdb20bc4bd3557872e5c59821114d51
UnioTrias <114605209+UnioTrias@users.noreply.github.com>
2023-03-13 12:48:20 +0100

    a

--- js/animacion.js
+++ js/animacion.js
@@ -5,20 +5,19 @@ function animacion(){
     Pepe.actualizarPosicion()
 
     moverFondo()
-
+  
     // esto al final siempre
     setTimeout(animacion, 50)
+
+    
   }
 
-  //Movimiento del fondo, se mueve 10% cada 100ms
+  //Movimiento del fondo, se mueve 20px cada 50ms
 var posicion=5 
 function moverFondo(){
     fondos = document.getElementById('pantalla');
 
         posicion -= 20;
         fondos.style.backgroundPosition=posicion+'px'
-
-    // setInterval(() => {
-    //     clearInterval(identificador);
-    // }, 5000);
 }
+
