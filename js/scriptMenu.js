4660facd1f590e868420c3b0da01243238720769
victice <127197524+victice@users.noreply.github.com>
2023-03-14 12:45:01 +0100

    pantalla pausa

--- js/scriptMenu.js
+++ js/scriptMenu.js
@@ -2,8 +2,14 @@ var menuDiv;
 var pantalla;
 var menuSup;
 
+function jugar1() {
+    menuDiv = document.getElementById('menuDiv');
+    pantallaPause = document.getElementById('pantallaPausa');
+    menuDiv.style.visibility = 'hidden';
+    pantallaPausa.hidden = false;
+}
 
-function jugar(){
+function jugar() {
 
     efectoDeSonido()
 
@@ -15,24 +21,25 @@ function jugar(){
     menuDiv = document.getElementById('menuDiv');
     pantalla = document.getElementById('pantalla');
     menuSup = document.getElementById('menuSup');
+    pantallaPausa = document.getElementById('pantallaPausa');
 
-    menuDiv.style.visibility='hidden';//Ponemos en hidden el DivMenu y se pone en visible la pantalla
-    pantalla.style.visibility='visible'
-    menuSup.style.visibility='visible'
+    pantallaPausa.style.visibility = 'hidden';//Ponemos en hidden el DivMenu y se pone en visible la pantalla
+    pantalla.style.visibility = 'visible'
+    menuSup.style.visibility = 'visible'
 
     var backgrounds = [// Se crea un array de los bg que se van a utilizar
-        'url(media/fondo1.jpg)', 
+        'url(media/fondo1.jpg)',
         'url(media/fondo2.jpg)',
         'url(media/fondo3.jpg)',
     ];
 
-    var inicioBackground = -1; 
+    var inicioBackground = -1;
     var intervalo = setInterval(cambiarBackground, 5000);// Se crea un setInterval para saber cada cuanto tiempo se van a cambiar los bg
 
     function cambiarBackground() {//La función comprueba si se han mostrado todas las imágenes de fondo definidas en la matriz backgrounds. Si todas las imágenes de fondo se han mostrado, la función clearInterval() se utiliza para detener el intervalo de tiempo. Si todavía hay 
         // imágenes de fondo disponibles, se cambia la imagen de fondo mediante la propiedad 
         if (inicioBackground == 2) {
-            clearInterval(intervalo); 
+            clearInterval(intervalo);
         } else {
             inicioBackground = (inicioBackground + 1) % backgrounds.length;
             document.getElementById('pantalla').style.backgroundImage = backgrounds[inicioBackground];
@@ -40,31 +47,31 @@ function jugar(){
     }
 }
 
-function ajustes(){
+function ajustes() {
 
     efectoDeSonido()
 
     var inAjustes = document.getElementById('inAjustes');
     menuDiv = document.getElementById('menuDiv');
 
-    menuDiv.style.visibility='hidden';
-    inAjustes.hidden=false;
+    menuDiv.style.visibility = 'hidden';
+    inAjustes.hidden = false;
 }
 
-function salir(){
+function salir() {
 
     efectoDeSonido()
 
     window.close();
 }
 
-function back(){
+function back() {
 
     efectoDeSonido()
 
     var inAjustes = document.getElementById('inAjustes');
 
-    inAjustes.hidden=true;
-    menuDiv.style='hidden=false';
+    inAjustes.hidden = true;
+    menuDiv.style = 'hidden=false';
 }
 
