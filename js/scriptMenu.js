5d5b7bdb9c64bb88c38f1e0e1c19b82f7294264f
IVANCSDO <114605010+IVANCSDO@users.noreply.github.com>
2023-03-14 01:19:55 +0100

    Añadido música de menú y tal

--- js/scriptMenu.js
+++ js/scriptMenu.js
@@ -1,12 +1,29 @@
 var menuDiv;
 var pantalla;
 
+var musicaMenuActiva=true
+// Para poner la música al iniciar la página, escucha cuando el ratón se mueve y comprueba si "musicaMenuActiva" es "true"
+document.addEventListener('mousemove', function() {
+    if(musicaMenuActiva==true){
+        var musicaMenu = document.getElementById("musicaMenu");
+        musicaMenu.play();
+        musicaMenu.volume = 0.30;
+    }
+
+  });
+
 function jugar(){
 
+    //con esto evito que se solapen las dos canciones, "desactivando" la reproduccion de la del menú
+    musicaMenuActiva=false
+    var musicaJuego = document.getElementById("musicaJuego")
+
+    document.getElementById("musicaMenu").pause()
+
     //reproducir la música del juego y ponerla a la mitad de volumen
-    document.getElementById("musicaJuego").volume = 0.5;
-    document.getElementById("musicaJuego").play();
-    document.getElementById("musicaJuego").loop=true;
+    musicaJuego.volume = 0.5;
+    musicaJuego.play();
+    musicaJuego.loop=true;
 
     //ejecuta el bucle para "animar" la nave
     animacion()
