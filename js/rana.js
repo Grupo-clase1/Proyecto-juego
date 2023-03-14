123fa9335f26ec9c06e9f5f0ad154089d1a39763
IVANCSDO <114605010+IVANCSDO@users.noreply.github.com>
2023-03-14 10:53:52 +0100

    el ovni circula por los carriles

--- js/rana.js
+++ js/rana.js
@@ -10,22 +10,30 @@ class nave {
         this.imagenMuerto = imagenMuerto;
 
         // carril en el que está
-        this.carril = 2;
+        this.carril = 3;
     }
 
     actualizarPosicion(){
 
         switch (this.carril) {
+            case 5:
+              this.positionY = "0%"
+              break;
+
+            case 4:
+              this.positionY = "20%"
+              break;
+
             case 3:
-                this.positionY = "0%"
+                this.positionY = "40%"
                 break;
 
             case 2:
-                this.positionY = "33%"
+                this.positionY = "60%"
                 break;
 
             case 1:
-                this.positionY = "66%"
+                this.positionY = "80%"
                 break;
         
             default:
@@ -50,7 +58,7 @@ let Pepe = new nave('Pepe','5%','33%','./media/ovni1p.png','./media/ovni1.gif','
 // Agregar un evento "keydown" al documento
 document.addEventListener("keydown", function(event) {
     if (event.key === "w" || event.key === "W") {
-      if (Pepe.carril < 3) { // Si la rana no está en el carril 3
+      if (Pepe.carril < 5) { // Si la rana no está en el carril 5
         Pepe.carril++; // Aumentar la propiedad "carril"
         // console.log(Pepe.carril)
       }
