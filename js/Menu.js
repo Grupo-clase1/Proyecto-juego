6b99a7ce476246acf2ec016da0df6e58179b1db1
UnioTrias <114605209+UnioTrias@users.noreply.github.com>
2023-03-17 09:43:38 +0100

    mas tiempo de cambio nivel

--- js/Menu.js
+++ js/Menu.js
@@ -46,7 +46,7 @@ function jugar(){
     ];
 
     var inicioBackground = -1; 
-    intervalo = setInterval(cambiarBackground, 4000);// Se crea un setInterval para saber cada cuanto tiempo se van a cambiar los bg
+    intervalo = setInterval(cambiarBackground, 10000);// Se crea un setInterval para saber cada cuanto tiempo se van a cambiar los bg
 
     function cambiarBackground() {//La función comprueba si se han mostrado todas las imágenes de fondo definidas en la matriz backgrounds. Si todas las imágenes de fondo se han mostrado, la función clearInterval() se utiliza para detener el intervalo de tiempo. Si todavía hay 
 
