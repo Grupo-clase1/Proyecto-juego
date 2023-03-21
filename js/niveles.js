1dc01743b4edb646d121261aca5744cb8366cd81
Rampayan <85584185+Rampayan@users.noreply.github.com>
2023-03-21 21:26:38 +0100

    rayan

--- js/niveles.js
+++ js/niveles.js
@@ -12,3 +12,34 @@ var nivel1 = new Nivel(1, 10, "media/fondo1.jpg");
 var nivel2 = new Nivel(2, 15, "media/fondo2.jpg");
 var nivel3 = new Nivel(3, 20, "media/fondo3.jpg");
 var nivel4 = new Nivel(4, 25, "media/fondo2.gif");
+
+function getPuntuacion() {
+  return parseInt(document.getElementById('contadorBackground').innerText);
+}
+
+function cambiarNivel() {
+  const puntuacion = getPuntuacion();
+  let nivelActual;
+
+  if (puntuacion >= 100) {
+    nivelActual = nivel3;
+  } else if (puntuacion >= 50) {
+    nivelActual = nivel2;
+  } else {
+    nivelActual = nivel1;
+  }
+
+  // Cambiar la velocidad del juego según el nivel
+  velocidad = nivelActual.velocidad;
+
+  // Cambiar el fondo del juego según el nivel
+  document.getElementById("pantalla").style.backgroundImage = `url(${nivelActual.fondo})`;
+
+  // Actualiza el texto del nivel en el menú superior
+  document.querySelector(".tituloNivel").innerHTML = `Nivel ${nivelActual.numero}<br><div id="vidas"><img id="vida1" src="media/vidas.png"><img id="vida2" src="media/vidas.png"><img id="vida3" src="media/vidas.png"></div><div id="contadorDinero"><img src="media/monedas.png" id="monedas">100<img src="media/billetes.png" id="billetes"></div>`;
+}
+
+
+
+
+
