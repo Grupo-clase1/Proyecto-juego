ea7ac0dc3544c54d11d5f88acab197dc1ef144a3
UnioTrias <114605209+UnioTrias@users.noreply.github.com>
2023-04-18 10:32:01 +0200

    vidas funcion aparte de nave

--- js/rana.js
+++ js/rana.js
@@ -14,7 +14,9 @@ class nave {
         this.imagenMuerto = imagenMuerto;
         this.id = id
         this.vidas=3
+        this.restarVida=restarVida
         this.monedas=100
+        this.masDinero=masDinero
 
         // carril en el que está
         this.carril = 3;
@@ -32,35 +34,40 @@ class nave {
  
     
   }
-
+}
   //esta funcion hay que unirla a las colisiones
-restarVida(){
-  vida3 = document.getElementById('vida3')
-  vida2 = document.getElementById('vida2')
-  vida1 = document.getElementById('vida1')
+  function restarVida(){
+    vida3 = document.getElementById('vida3')
+    vida2 = document.getElementById('vida2')
+    vida1 = document.getElementById('vida1')
 
-  this.vidas --;
+    this.vidas --;
 
-  if (this.vidas==2){
-    vida3.style.visibility = 'hidden';
-  }else if (this.vidas == 1){
-    vida3.style.visibility = 'hidden';
-    vida2.style.visibility = 'hidden';
-  }else if (this.vidas <= 0){           //aviso cuando se gastan las vidas
-    vida1.style.visibility = 'hidden';
-    pausa=true;
-    var texto=''
-    texto+='<div id="mensaje">';
-		texto+='<br /><br /><br />GAME OVER';
-		texto+='<br /><br /><input type="button" value="Reiniciar" onclick="reiniciar();" style="font-size: 25px;"/>';
-		texto+='</div>';
-		document.getElementById('aviso').innerHTML+=texto;
-  }
+    if (this.vidas==2){
+      vida3.style.visibility = 'hidden';
+    }else if (this.vidas == 1){
+      vida3.style.visibility = 'hidden';
+      vida2.style.visibility = 'hidden';
+    }else if (this.vidas <= 0){           //aviso cuando se gastan las vidas
+      vida1.style.visibility = 'hidden';
+      pausa=true;
+      var texto=''
+      texto+='<div id="mensaje">';
+      texto+='<br /><br /><br />GAME OVER';
+      texto+='<br /><br /><input type="button" value="Reiniciar" onclick="reiniciar();" style="font-size: 25px;"/>';
+      texto+='</div>';
+      document.getElementById('aviso').innerHTML+=texto;
+    }
+}
+
+function masDinero(){
+  //  var dinero = document.getElementById('contadorDinero')
+
+  this.monedas +=50
+  
 }
 
 
-
-}
 //Solo puede haber un objeto nave y desde este cambiar las propiedades.
 let Pepe = new nave('Pepe','5%','40%','./media/ovni1p.png','./media/ovni1.gif','./media/muerto.gif', "rana");
 let Pepe1 = new nave('Pepe1','5%','40%','./media/ovni2p.png','./media/ovni2.gif','./media/muerto.gif', "rana");
